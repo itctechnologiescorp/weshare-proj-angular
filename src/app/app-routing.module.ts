08aec9cdeb41dc6af389e927b09aa428e81fc6b9
Vivekraj <viveks.it08@gmail.com>
2019-08-06 07:07:57 +0530

    signInSignUp file update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Templates/home/home.component';
 import { SelectionComponent } from './Templates/selection/selection.component';
 import { LoginComponent } from './Templates/auth/login/login.component';
+import { SignUpComponent } from './Templates/sign-up/sign-up.component';
+import { SignInComponent } from './Templates/sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './Templates/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   {
@@ -16,6 +19,18 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'signUp',
+    component: SignUpComponent
+  },
+  {
+    path: 'signIn',
+    component: SignInComponent
+  },
+  {
+    path: 'forgotPassword',
+    component: ForgotPasswordComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './Templates/auth/login/login.component';
 import { HomeComponent } from './Templates/home/home.component';
 import { SelectionComponent } from './Templates/selection/selection.component';
+import { SignInComponent } from './Templates/sign-in/sign-in.component';
+import { SignUpComponent } from './Templates/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './Templates/forgot-password/forgot-password.component';
 
 
 @NgModule({
@@ -15,7 +18,10 @@ import { SelectionComponent } from './Templates/selection/selection.component';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    SelectionComponent
+    SelectionComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     CommonModule,
