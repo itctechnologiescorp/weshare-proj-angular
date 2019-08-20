54e29fc042ccc1665a3287d9361e2b7b2371edae
davidantony1987 <davidantony1987@gmail.com>
2019-08-20 19:56:16 +0000

    Updated get last user id service

--- src/app/Templates/auth/auth.service.ts
+++ src/app/Templates/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 @Injectable()
 export class AuthService {
     //url = 'http://localhost:3000/create';
-    url = 'http://ec2-13-232-139-118.ap-south-1.compute.amazonaws.com:3000/profile/create';
+    url = 'http://ec2-13-235-9-150.ap-south-1.compute.amazonaws.com:3000/';
     constructor(private http: HttpClient) { }
 
     createUser(data: any) { console.log('data', data);
@@ -13,7 +13,11 @@ export class AuthService {
         const options = {
             headers: httpHeaders
         };        
-        return this.http.post<any>(this.url, data, options);
-    }    
+        return this.http.post<any>(this.url+'profile/create', data, options);
+    } 
+    
+    getLastUserId() { 
+        return this.http.get(this.url+'profile/getLastUserId');
+    } 
       
 } 
--- src/app/Templates/sign-up/sign-up.component.ts
+++ src/app/Templates/sign-up/sign-up.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from '../../Templates/auth/auth.service';
 export class SignUpComponent implements OnInit {
 
 signupForm: FormGroup;
-Key: number = 100001;
+Key: number = 0;
 Region: string = 'IND';
 Lang: string = 'EN';
 Name: string = '';
@@ -34,7 +34,8 @@ Model: string = '';
 RegNo: string = ''; 
 RCBookCopy: string = '';
 successMsg: boolean;
-submitted = false; 
+submitted = false;
+userId: number; 
 
 constructor(private frmbuilder: FormBuilder,
             private authService: AuthService)  
@@ -43,35 +44,48 @@ constructor(private frmbuilder: FormBuilder,
       Key: new FormControl(100002),
       Region: new FormControl('IND'),
       Lang: new FormControl('EN'),  
-      Name: new FormControl('', Validators.required),
+      Name: new FormControl(null, Validators.required),
       isProvider: new FormControl('FALSE'),
       isCar: new FormControl('FALSE'),
       Gender: new FormControl('M'),
-      EmailId: new FormControl('', Validators.required),
-      PhoneNo: new FormControl('', Validators.required),
-      Employer: new FormControl('', Validators.required),
-      Idproof: new FormControl('', Validators.required),
-      Addr1: new FormControl('', Validators.required),
-      Addr2: new FormControl(),
-      Country: new FormControl('', Validators.required),
-      State: new FormControl('', Validators.required),
-      City: new FormControl('', Validators.required),
-      Zip: new FormControl('', Validators.required),
-      isPetrol: new FormControl('', Validators.required),
-      Mileage: new FormControl('', Validators.required),
-      Make: new FormControl('', Validators.required),
-      Model: new FormControl('', Validators.required),
-      RegNo: new FormControl('', Validators.required),
-      RCBookCopy: new FormControl('', Validators.required),
-      vehicleDetails: new FormControl('', Validators.required)
+      EmailId: new FormControl(null, Validators.required),
+      PhoneNo: new FormControl(null, Validators.required),
+      Employer: new FormControl(null, Validators.required),
+      Idproof: new FormControl(null, Validators.required),
+      Addr1: new FormControl(null, Validators.required),
+      Addr2: new FormControl(null),
+      Country: new FormControl(null, Validators.required),
+      State: new FormControl(null, Validators.required),
+      City: new FormControl(null, Validators.required),
+      Zip: new FormControl(null, Validators.required),
+      isPetrol: new FormControl(null, Validators.required),
+      Mileage: new FormControl(null, Validators.required),
+      Make: new FormControl(null, Validators.required),
+      Model: new FormControl(null, Validators.required),
+      RegNo: new FormControl(null, Validators.required),
+      RCBookCopy: new FormControl(null, Validators.required),
+      vehicleDetails: new FormControl(null, Validators.required)
     });  
    }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.authService.getLastUserId().subscribe((res: any)=>{
+       if (res.status === 200) {
+        const data = res.data;
+        this.userId = Number(data.Key) + 1;
+        
+      } else {
+        this.userId = 100000;
+      }
+    })
+    
   }
 
 
   PostData(signupForm) {
+    this.signupForm.value.Key = this.userId;
+    this.signupForm.value.Idproof = null;
+    this.signupForm.value.RCBookCopy = null;
     if(this.signupForm.invalid){
       console.log('invalid')
       this.submitted = true;
@@ -79,6 +93,7 @@ constructor(private frmbuilder: FormBuilder,
       console.log('form', signupForm.value);
       this.submitted = true;
       this.authService.createUser(signupForm.value).subscribe((res: any) => {
+        console.log('res', res);
         if (res.status === 200) {
           this.successMsg = true;
         } else {
